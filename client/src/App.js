c5756b805b06f2452069fad9ef3809af9e714e2c
Edson Ayllon <eayllon1@gmail.com>
2020-11-14 00:45:25 -0500

    configure

---
+++ client/src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+import { Button } from 'antd';
+
+function App() {
+  return (
+    <div>
+      
+      <div className="App">
+      
+
+      <Button type="primary">
+          Learn React
+      </Button>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        
+        <button>other</button>
+
+    </div>
+    </div>
+    
+  );
+}
+
+export default App;
